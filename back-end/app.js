df1afde48d5682efcf5bfc400ab351571cf668b9
ewenlarrieu <ewenlarrieu200515@gmail.com>
2025-11-25 11:28:16 +0100

    Probleme cors resolu

--- back-end/app.js
+++ back-end/app.js
@@ -10,7 +10,14 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/programmes", programmeRoutes);
