a359ce2efc238cd197181a5fc782e3f2e7bf334e
ewenlarrieu <ewenlarrieu200515@gmail.com>
2025-12-08 16:00:37 +0100

    Correction import de Home.scss

--- Front-end/src/pages/Home.jsx
+++ Front-end/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 ﻿import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Logo from '../components/Logo';
-import '../style/Hone.scss';
+import '../style/Home.scss';
 
 export default function Home() {
   const navigate = useNavigate();
